a58730c6caf78b56e8efcb263ca225f9f7e321d3
Vikas Mali <vikasmali4303@gmail.com>
2022-07-14 08:50:34 +0530

    Updates

---
+++ precache-manifest.bdeb7c9d3abd9c4deeb589197f495199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84046bcab88d7a1eedaf",
+    "url": "/codemastery/static/css/main.d4f4e7fb.chunk.css"
+  },
+  {
+    "revision": "84046bcab88d7a1eedaf",
+    "url": "/codemastery/static/js/main.84046bca.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/codemastery/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "601affd8a5bb6b5a29af",
+    "url": "/codemastery/static/js/2.601affd8.chunk.js"
+  },
+  {
+    "revision": "bbef27f591f4bb0e6797",
+    "url": "/codemastery/static/js/runtime~main.bbef27f5.js"
+  },
+  {
+    "revision": "8fc702123a04e579324f600e48a9f720",
+    "url": "/codemastery/static/media/herosectionbg.8fc70212.jpg"
+  },
+  {
+    "revision": "0cf402e7b5f757e1aa52bf88bfa3b169",
+    "url": "/codemastery/index.html"
+  }
+];
